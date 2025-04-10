218d9d75bd624931a29aac0d2ba6d94ea291e6b2
NoamArtstain <noam.artstain@gmail.com>
2025-04-10 10:48:00 +0300

    thali is king
    blah

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 from solar_energy_forecast.preprocess import get_data
 
-
+#Yahli the king
 def main():
     df = get_data(verbose=True)
     df.to_csv("output/complete_data.csv")
